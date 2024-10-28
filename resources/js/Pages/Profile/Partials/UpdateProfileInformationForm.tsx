ed815be777a126ab7c7deacda414ccac4d982b32
Gaks3 <dwi66116@gmail.com>
2024-10-28 21:35:28 +0700

    feat: add update profile image form

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
@@ -2,9 +2,11 @@ import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
+import { type UpdateProfileSchema } from "@/types/user";
 import { Transition } from "@headlessui/react";
 import { Link, useForm, usePage } from "@inertiajs/react";
-import { FormEventHandler } from "react";
+import { ChangeEvent, FormEventHandler, useRef } from "react";
+import { BsCamera } from "react-icons/bs";
 
 export default function UpdateProfileInformation({
   mustVerifyEmail,
@@ -16,38 +18,84 @@ export default function UpdateProfileInformation({
   className?: string;
 }) {
   const user = usePage().props.auth.user;
+  const avatarRef = useRef<HTMLImageElement | null>(null);
 
-  const { data, setData, patch, errors, processing, recentlySuccessful } =
-    useForm({
+  const { data, setData, post, errors, processing, recentlySuccessful } =
+    useForm<UpdateProfileSchema>({
+      _method: "PATCH",
       name: user.name,
       email: user.email,
+      avatar: null,
     });
 
   const submit: FormEventHandler = (e) => {
     e.preventDefault();
 
-    patch(route("profile.update"));
+    post(route("profile.update"));
+  };
+
+  const changeAvatar = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      setData("avatar", files[0]);
+
+      const imageUrl = window.URL.createObjectURL(files[0]);
+      avatarRef.current?.setAttribute("src", imageUrl);
+
+      return () => {
+        window.URL.revokeObjectURL(imageUrl);
+      };
+    }
   };
 
   return (
     <section className={className}>
       <header>
-        <h2 className="text-lg font-medium text-gray-900">
+        <h2 className="text-lg font-medium text-foreground">
           Profile Information
         </h2>
 
-        <p className="mt-1 text-sm text-foreground">
+        <p className="mt-1 text-sm text-secondary-foreground">
           Update your account's profile information and email address.
         </p>
       </header>
 
-      <form onSubmit={submit} className="mt-6 space-y-6">
+      <form
+        onSubmit={submit}
+        className="mt-6 space-y-6"
+        encType="multipart/form-data"
+      >
+        <div className="relative picture">
+          <img
+            src={user.avatar}
+            alt={user.name}
+            className="mx-auto border rounded-full size-20 border-secondary"
+            ref={avatarRef}
+          />
+
+          <label
+            htmlFor="avatar"
+            className="absolute flex items-center justify-center px-2 translate-x-5 rounded-full cursor-pointer btn btn-primary left-1/2 top-6"
+            tabIndex={0}
+          >
+            <BsCamera />
+          </label>
+          <input
+            type="file"
+            name="avatar"
+            id="avatar"
+            onChange={changeAvatar}
+            className="hidden"
+          />
+        </div>
+
         <div>
           <InputLabel htmlFor="name" value="Name" />
 
           <TextInput
             id="name"
-            className="mt-1 block w-full"
+            className="block w-full mt-1"
             value={data.name}
             onChange={(e) => setData("name", e.target.value)}
             required
@@ -64,7 +112,7 @@ export default function UpdateProfileInformation({
           <TextInput
             id="email"
             type="email"
-            className="mt-1 block w-full"
+            className="block w-full mt-1"
             value={data.email}
             onChange={(e) => setData("email", e.target.value)}
             required
@@ -76,20 +124,20 @@ export default function UpdateProfileInformation({
 
         {mustVerifyEmail && user.email_verified_at === null && (
           <div>
-            <p className="mt-2 text-sm text-gray-800">
+            <p className="mt-2 text-sm text-foreground">
               Your email address is unverified.
               <Link
                 href={route("verification.send")}
                 method="post"
                 as="button"
-                className="rounded-md text-sm text-foreground underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="btn btn-secondary"
               >
                 Click here to re-send the verification email.
               </Link>
             </p>
 
             {status === "verification-link-sent" && (
-              <div className="mt-2 text-sm font-medium text-green-600">
+              <div className="mt-2 text-sm font-medium text-success">
                 A new verification link has been sent to your email address.
               </div>
             )}
