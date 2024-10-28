8bee2125e35d32e644ae2579e7749a8644f4769e
Gaks3 <dwi66116@gmail.com>
2024-10-28 21:32:56 +0700

    feat: change edit profile page style

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -20,8 +20,8 @@ export default function Edit({
       <Head title="Profile" />
 
       <div className="py-4 sm:py-6">
-        <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-          <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+        <div className="px-4 mx-auto space-y-4 max-w-7xl sm:space-y-6 sm:px-6">
+          <div className="p-4 rounded-lg shadow bg-background sm:p-8">
             <UpdateProfileInformationForm
               mustVerifyEmail={mustVerifyEmail}
               status={status}
@@ -29,11 +29,11 @@ export default function Edit({
             />
           </div>
 
-          <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+          <div className="p-4 rounded-lg shadow bg-background sm:p-8">
             <UpdatePasswordForm className="max-w-xl" />
           </div>
 
-          <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+          <div className="p-4 rounded-lg shadow bg-background sm:p-8">
             <DeleteUserForm className="max-w-xl" />
           </div>
         </div>
