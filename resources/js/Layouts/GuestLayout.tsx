9138192166febe2206a3e899d2fdb5764f428316
Gaks3 <dwi66116@gmail.com>
2024-10-29 13:02:53 +0700

    feat: add guest layout style

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,10 +4,10 @@ import { PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-secondary">
-      <div className="flex flex-col w-full max-w-md p-6 my-10 space-y-6 overflow-hidden rounded-lg shadow-md bg-background">
-        <Link href="/" className="w-20 mx-auto">
-          <ApplicationLogo className="w-20 h-20 text-gray-500 fill-current" />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-secondary">
+      <div className="my-10 flex w-11/12 max-w-md flex-col space-y-6 overflow-hidden rounded-lg bg-background p-6 shadow-md">
+        <Link href="/" className="mx-auto w-20">
+          <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
         </Link>
         {children}
       </div>
