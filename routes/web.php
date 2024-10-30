a1b5c496d99ff0e547ad4baeeb75a50b93b88cdd
Gaks3 <dwi66116@gmail.com>
2024-10-30 21:19:25 +0700

    feat: change route chats to use chats controller and add contacts, archived chats route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ChatsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [HomeController::class,'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -16,7 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/chats', fn () => '')->name('chats.index');
+    Route::get('/chats', [ChatsController::class, 'index'])->name('chats.index');
+    Route::get('/contacts', [ChatsController::class, 'index'])->name('contacts.index');
+    Route::get('/archived_chats', [ChatsController::class, 'index'])->name('archived_chats.index');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
