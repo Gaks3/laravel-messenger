3af33f5dee5d7953d17b44e1746bff812d1d1fa9
Gaks3 <dwi66116@gmail.com>
2024-10-30 21:20:41 +0700

    refactor: move type user to auth object key

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,8 +4,6 @@ import { type User } from "./user";
 export type PageProps<
   T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
-  auth: {
-    user: User;
-  };
+  auth: User;
   ziggy: Config & { location: string };
 };
