5b9015ce34ef7e50a6d31e4f071a45ba399175ae
Gaks3 <dwi66116@gmail.com>
2024-10-31 22:09:56 +0700

    feat: create content empty component style

--- resources/js/Components/Chats/ContentEmpty.tsx
+++ resources/js/Components/Chats/ContentEmpty.tsx
@@ -1,3 +1,12 @@
 export default function ContentEmpty() {
-  return <div>ContentEmpty</div>;
+  return (
+    <div className="flex-col items-center justify-center flex-1 order-3 hidden w-full h-screen gap-4 border-l border-secondary sm:flex">
+      <img
+        src="/images/message-empty.png"
+        alt="Message Empty Image"
+        className="w-[250px]"
+      />
+      <h5 className="text-lg font-medium">No chats selected</h5>
+    </div>
+  );
 }
